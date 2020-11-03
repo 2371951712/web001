f833ce3d09df5a34c44032874485511ef3a22fdd
2371951712 <2371951712@qq.com>
2020-11-03 11:41:01 +0800

    caiwang

---
+++ 第十三次课/caiwang.js
@@ -0,0 +1,8 @@
+function caiwang(){
+    var Array = [12,67,89,100,34,56,78,99];
+    for(let index = 0; index < Array.length; index++){
+        const element = Array[index];
+        console.log(element);
+    }
+}
+caiwang();
